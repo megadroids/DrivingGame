010339ed38c149c33ebbfe9a7101be39bc00453c
Kiruthiga <kiru.muthu@gmail.com>
2017-11-24 00:27:50 +0100

    commit with game logic and player details

--- app/src/main/java/megadroid/drivinggame/view/GameView.java
+++ app/src/main/java/megadroid/drivinggame/view/GameView.java
@@ -1,8 +1,135 @@
 package megadroid.drivinggame.view;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+
+import megadroid.drivinggame.model.Player;
+
 /**
- * Created by megadroids on 11/23/2017.
+ * Created by Kiruthiga on 11/22/2017.
  */
 
-public class GameView {
+public class GameView extends SurfaceView implements Runnable {
+
+    //boolean variable to track if the game is playing or not
+    volatile boolean playing;
+
+    //the game thread
+    private Thread gameThread = null;
+
+    //adding the player to this class
+    private Player player;
+
+    //These objects will be used for drawing
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder surfaceHolder;
+
+    //Class constructor
+    public GameView(Context context, int screenX, int screenY) {
+        super(context);
+
+        //initializing player object
+        //this time also passing screen size to player constructor
+        player = new Player(context, screenX, screenY);
+
+        //initializing drawing objects
+        surfaceHolder = getHolder();
+        paint = new Paint();
+    }
+
+    @Override
+    public void run() {
+        while (playing) {
+            //to update the frame
+            update();
+
+            //to draw the frame
+            draw();
+
+            //to control
+            control();
+        }
+    }
+
+
+    private void update() {
+        //updating player position
+        player.update();
+
+    }
+
+    private void draw() {
+        //checking if surface is valid
+        if (surfaceHolder.getSurface().isValid()) {
+            //locking the canvas
+            canvas = surfaceHolder.lockCanvas();
+            //drawing a background color for canvas
+            canvas.drawColor(Color.BLACK);
+            //Drawing the player
+            canvas.drawBitmap(
+                    player.getBitmap(),
+                    player.getX(),
+                    player.getY(),
+                    paint);
+            //Unlocking the canvas
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+
+    }
+
+    private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pause() {
+        //when the game is paused
+        //setting the variable to false
+        playing = false;
+        try {
+            //stopping the thread
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void resume() {
+        //when the game is resumed
+        //starting the thread again
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                //When the user presses on the screen
+                //stopping the boosting when screen is released
+                player.setBoosting();
+                break;
+
+
+            case MotionEvent.ACTION_DOWN:
+                //When the user releases the screen
+                //boosting the space jet when screen is pressed
+                player.stopBoosting();
+                break;
+
+        }
+        return true;
+    }
 }
