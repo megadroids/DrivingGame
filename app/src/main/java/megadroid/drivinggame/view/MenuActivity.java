e7993bac50a66f7ad0d3f0f1294bba5dc10017ef
Elena Kurganova <elena.kurganova@yahoo.com>
2017-11-24 20:38:04 +0100

    Add MenuActivity and activity_menu

--- app/src/main/java/megadroid/drivinggame/view/MenuActivity.java
+++ app/src/main/java/megadroid/drivinggame/view/MenuActivity.java
@@ -2,14 +2,69 @@ package megadroid.drivinggame.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import megadroid.drivinggame.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //Crete image buttons
+        ImageButton playButton;
+        ImageButton shopButton;
+        ImageButton muteSoundButton;
+        ImageButton exitButton;
+
+        //set the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //getting the buttons
+        playButton = (ImageButton) findViewById(R.id.buttonPlay);
+        shopButton = (ImageButton) findViewById(R.id.buttonShop);
+        muteSoundButton = (ImageButton) findViewById(R.id.muteSound);
+        exitButton = (ImageButton) findViewById(R.id.exit);
+
+        //adding a click listener to buttons
+        playButton.setOnClickListener(this);
+        shopButton.setOnClickListener(this);
+        muteSoundButton.setOnClickListener(this);
+        exitButton.setOnClickListener(this);
+    }
+
+    // the onclick methods to handle clicking different buttons
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            //the transition from MenuActivity to GameActivity
+            case R.id.buttonPlay:
+                startActivity(new Intent(MenuActivity.this, GameActivity.class));
+                break;
+
+            //the transition from MenuActivity to ShopActivity
+            case R.id.buttonShop:
+                startActivity(new Intent(MenuActivity.this, ShopActivity.class));
+                break;
+
+            case R.id.muteSound:
+                break;
+
+            case R.id.exit:
+                Intent startMain = new Intent(Intent.ACTION_MAIN);
+                startMain.addCategory(Intent.CATEGORY_HOME);
+                startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(startMain);
+                break;
+
+            default:
+                break;
+        }
     }
 }
