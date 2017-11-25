67c4831b286c667a14cddb96aa3faada6b272daa
jiwendi <joyiwendi@yahoo.com>
2017-11-25 07:55:32 +0100

    Scrolling Background created for the game. Background class created and edits to the GameView class was made.

--- app/src/main/java/megadroid/drivinggame/view/Background.java
+++ app/src/main/java/megadroid/drivinggame/view/Background.java
@@ -3,6 +3,42 @@ package megadroid.drivinggame.view;
 /**
  * Created by Megadroids on 11/24/2017.
  */
+   import android.graphics.Bitmap;
+   import android.graphics.Canvas;
+
+/**
+ * Created by joyag on 24/11/2017.
+ */
 
 public class Background {
+    private Bitmap image;
+    private int x, y, dy;
+
+    public Background(Bitmap res){
+
+        image = res;
+    }
+
+    public void update(){
+
+        y+=dy;
+        if(y<-GameView.HEIGHT) {
+            y = 0;
+        }
+
+    }
+
+    public void draw(Canvas canvas){
+
+        canvas.drawBitmap(image, x, y, null);
+        if(y<0){
+            canvas.drawBitmap(image, x, y+GameView.HEIGHT, null);
+        }
+    }
+
+    public void setVector(int dy){
+        this.dy = dy;
+
+
+    }
 }
