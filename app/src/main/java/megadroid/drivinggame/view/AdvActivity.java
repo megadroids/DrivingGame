4b6a2b68137694d8fcd2575aa30d1094417a5dec
joaokeselik <keselik@kth.se>
2017-11-27 16:22:48 +0100

    New branch

---
+++ app/src/main/java/megadroid/drivinggame/view/AdvActivity.java
@@ -0,0 +1,18 @@
+package megadroid.drivinggame.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import megadroid.drivinggame.R;
+
+public class AdvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adv);
+
+    }
+
+
+}
