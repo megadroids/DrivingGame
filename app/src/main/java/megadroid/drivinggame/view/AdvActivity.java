b3df3f82038488432547a06e53f2692ce1d1cf12
joaokeselik <keselik@kth.se>
2017-11-29 15:38:45 +0100

    Compiled google ads Launching MainActivity that calls AdvActivity Added button, that calls rewarded video, to AdvActivity

--- app/src/main/java/megadroid/drivinggame/view/AdvActivity.java
+++ app/src/main/java/megadroid/drivinggame/view/AdvActivity.java
@@ -2,17 +2,116 @@ package megadroid.drivinggame.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import megadroid.drivinggame.R;
 
-public class AdvActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+public class AdvActivity extends AppCompatActivity implements RewardedVideoAdListener {
+    private RewardedVideoAd mRewardedVideoAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adv);
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
 
+        loadRewardedVideoAd();
     }
 
+    //admob code with real ad unit ID (code to be used)
+    /*
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-4000901278297592/6305690228",
+                new AdRequest.Builder().build());
+    }
+    */
+
+    //test code with test ad unit ID
+    /*
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+    }
+    */
+
+    //test code with real ad unit ID and bluestacks test device ID
+    private void loadRewardedVideoAd() {
+        AdRequest request = new AdRequest.Builder()
+                .addTestDevice("759B17904BBE87AF2336E949621B3938")
+                .build();
+        mRewardedVideoAd.loadAd("ca-app-pub-4000901278297592/6305690228",
+                request);
+    }
+
+    @Override
+    public void onRewarded(RewardItem reward) {
+        Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount(), Toast.LENGTH_SHORT).show();
+        // Reward the user.
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "onRewardedVideoAdLeftApplication",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+        loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int errorCode) {
+        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        mRewardedVideoAd.resume(this);
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mRewardedVideoAd.pause(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mRewardedVideoAd.destroy(this);
+        super.onDestroy();
+    }
+
+    public void showAdv(View view){
+        if (mRewardedVideoAd.isLoaded()) {
+            mRewardedVideoAd.show();
+        }
+    }
 
 }
--- app/src/main/java/megadroid/drivinggame/view/MainActivity.java
+++ app/src/main/java/megadroid/drivinggame/view/MainActivity.java
@@ -1,7 +1,11 @@
 package megadroid.drivinggame.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.MobileAds;
 
 import megadroid.drivinggame.R;
 
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //admob code
+        MobileAds.initialize(this, "ca-app-pub-4000901278297592~9973472497");
+    }
+
+    //test code
+    public void callAdvActivity(View view){
+        Intent myIntent = new Intent(MainActivity.this, AdvActivity.class);
+
+        MainActivity.this.startActivity(myIntent);
     }
 }
