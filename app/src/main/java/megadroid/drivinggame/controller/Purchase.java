0d1cb5a670abffc9c964f01e434fbe50b2aa3360
molock10 <molock10@hotmail.com>
2017-12-01 17:58:51 +0100

    Initial shop logic

--- app/src/main/java/megadroid/drivinggame/controller/Purchase.java
+++ app/src/main/java/megadroid/drivinggame/controller/Purchase.java
@@ -1,8 +1,139 @@
 package megadroid.drivinggame.controller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by megadroids on 11/23/2017.
  */
 
 public class Purchase {
+
+    private ScoreMonitor scoreMonitor;
+    private String currentCar;
+    private String currentTheme;
+    private int points;
+    private ArrayList<String> carlist;
+    private ArrayList<String> themelist;
+
+    private SharedPreferences sharedPreferences;
+
+
+    public Purchase(Context context, String screenType) throws JSONException {
+
+        sharedPreferences = context.getSharedPreferences("Prices",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("SecondCar", 0);
+        editor.putInt("SecondTheme", 1000);
+        editor.commit();
+
+        scoreMonitor = new ScoreMonitor();
+        scoreMonitor.readJSON(context, screenType);
+
+        currentCar = scoreMonitor.getCurrentCar();
+        currentTheme = scoreMonitor.getCurrentTheme();
+        points = scoreMonitor.getPoints();
+        carlist = scoreMonitor.getCarlist();
+        themelist = scoreMonitor.getThemelist();
+    }
+
+
+    public boolean isCarBought(String id) {
+        Iterator<String> it = themelist.iterator();
+        while(it.hasNext()) {
+            if (it.next().equals(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isThemeBought(String id){
+        Iterator<String> it = themelist.iterator();
+        while(it.hasNext()){
+            if(it.next().equals(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the user has enough points to purchase the item selected
+     *
+     * @param itemToPurchase, the name of the item1 to check if affordable
+     * @return true, if the user can afford the car chosen
+     */
+    public boolean itemAffordable(String itemToPurchase){
+        int price = sharedPreferences.getInt(itemToPurchase, 0);
+            if(points >= price){
+                return true;
+            }else{
+                return false;
+            }
+    }
+
+    public void purchaseCar(String carToPurchase){
+            int price = sharedPreferences.getInt(carToPurchase, 0);
+            carlist.add(carToPurchase);
+            points -= price;
+    }
+
+    public void purchaseTheme(String themeToPurchase){
+        int price = sharedPreferences.getInt(themeToPurchase, 0);
+        points -= price;
+        themelist.add(themeToPurchase);
+    }
+
+
+
+
+
+    public int getPoints(){
+        return points;
+    }
+
+    public void closeShop(Context context) throws JSONException{
+
+        scoreMonitor.writeJSON(context,-1,points,carlist,themelist,currentCar,currentTheme);
+    }
+
+
+
+    //Checkers and setters
+    /**
+     * Checks if the car is the selected car for play
+     *
+     * @param carPressed, the car to check if it has been chosen by the user
+     * @return true, if the car is in use as the playable car, else false
+     */
+    public boolean carSelected(String carPressed){
+        return currentCar.equals(carPressed);
+    }
+
+    /**
+     * Checks if the car is the selected car for play
+     *
+     * @param themePressed, the theme to check if it has been chosen by the user
+     * @return true, if the car is in use as the playable car, else false
+     */
+    public boolean themeSelected(String themePressed){
+        return currentTheme.equals(themePressed);
+    }
+
+    public void selectCar(String newcurrentCar) {
+        this.currentCar = newcurrentCar;
+    }
+
+    public void selectTheme(String newCurrentTheme){
+        this.currentTheme = newCurrentTheme;
+    }
+
 }
--- app/src/main/java/megadroid/drivinggame/view/ShopActivity.java
+++ app/src/main/java/megadroid/drivinggame/view/ShopActivity.java
@@ -1,6 +1,25 @@
 package megadroid.drivinggame.view;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import megadroid.drivinggame.R;
+import megadroid.drivinggame.controller.Purchase;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,86 +29,133 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import megadroid.drivinggame.R;
 import megadroid.drivinggame.controller.ScoreMonitor;
 import java.util.ArrayList;
 
-public class ShopActivity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int points;
-    private ScoreMonitor monitor;
-    private ArrayList<String> carlist;
-    private ArrayList<String> themelist;
-    private String currentCar;
-    private String currentTheme;
+    private Purchase purchaser;
+    private List<ImageButton> carButtons;
+    private List<ImageButton> themeButtons;
+
+    private final String FIRST_CAR_IDENTIFIER = "FirstCar";
+    private final String SECOND_CAR_IDENTIFIER = "SecondCar";
+    private final String FIRST_THEME_IDENTIFIER = "FirstTheme";
+    private final String SECOND_THEME_IDENTIFIER = "SecondTheme";
+
+    HashMap<String, Integer> alternativeImages;
+    HashMap<Integer, String> intIdToString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
-        monitor = new ScoreMonitor();
-        carlist = new ArrayList<String>();
-        themelist = new ArrayList<String>();
+        alternativeImages = new HashMap<>();
+        alternativeImages.put("FirstCar", R.drawable.def_car);
+        alternativeImages.put("SecondCar", R.drawable.car3_unlocked);
+        alternativeImages.put("ThirdCar", R.drawable.rocket_unlocked);
 
-
-        //read points, cars and themes from JSON file
-        readJson();
-
-        //toDo: cars , themes and updated points should be written from the method of buy button click
-        //toDo: but highscore should not be updated from ShopActivity so pass -1 as given below
-        int highscore = -1;
-        // points = 800;
-        //ArrayList<String> cars = new ArrayList<String>(); //{"01", "02", "03"};
-        carlist.add("01");
-        carlist.add("02");
-        carlist.add("03");
-
-        //ArrayList<String> themes = new ArrayList<String>(); //{"christmas.png", "farm.png", "city.png"};
-        themelist.add("christmas.png");
-        themelist.add("farm.png");
-        themelist.add("city.png");
-
-        currentCar="01";
-        currentTheme="farm.png";
-
+        intIdToString = new HashMap<>();
+        intIdToString.put(R.id.firstCar, "FirstCar");
+        intIdToString.put(R.id.secondCar, "SecondCar");
+        intIdToString.put(R.id.thirdCar, "ThirdCar");
+        intIdToString.put(R.id.fourthCar, "FourthCar");
 
         try {
-            monitor.writeJSON(this, highscore, points, carlist, themelist, currentCar,currentTheme);
-        } catch (JSONException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            purchaser = new Purchase(this, "Shop");
+        }catch (JSONException e){
+            Toast.makeText(this, "Problem loading from the database", Toast.LENGTH_SHORT);
         }
+        carButtons = new ArrayList<>();
+        themeButtons = new ArrayList<>();
+
+        initializeButtons();
     }
 
-    private void readJson() {
-        //read scores from JSON file
-        try {
+    private void redrawScreen() {
+        TextView pointsView = (TextView) findViewById(R.id.points);
+        pointsView.setText(Integer.toString(purchaser.getPoints()));
 
-            String values = monitor.readJSON(this.getApplicationContext(),"Shop");
-
-            if (values.isEmpty()) {
-                Toast.makeText(this, "No Scores", Toast.LENGTH_LONG).show();
-            } else {
-
-                points = monitor.getPoints();
-                carlist = monitor.getCarlist();
-                themelist = monitor.getThemelist();
-                currentCar =monitor.getCurrentCar();
-                currentTheme = monitor.getCurrentTheme();
+        //Setting the appropriate images for each car button item
+        for (ImageButton imageButton : carButtons) {
+            String carName = intIdToString.get(imageButton.getId());
+            if (purchaser.carSelected(carName)) {
+                imageButton.setImageResource(alternativeImages.get(carName));
+                imageButton.setBackgroundResource(R.color.purple);
             }
-
-            //Toast.makeText(this,textToPrint,Toast.LENGTH_LONG).show();
-
-
-        } catch (JSONException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            else if (purchaser.isCarBought(carName)) {
+                imageButton.setImageResource(alternativeImages.get(carName));
+                imageButton.setBackgroundResource(R.color.colorAccent);
+            }
+            else{
+                imageButton.setBackgroundResource(R.color.colorAccent);
+            }
         }
+    }
+    private void initializeButtons(){
 
-
+        carButtons.add((ImageButton) findViewById(R.id.firstCar));
+        carButtons.add((ImageButton) findViewById(R.id.secondCar));
+        carButtons.add((ImageButton) findViewById(R.id.thirdCar));
+        for(ImageButton imageButton : carButtons){
+            imageButton.setOnClickListener(this);
+        }
+        //for(ImageButton imageButton : themeButtons){
+        //    imageButton.setOnClickListener(this);
+        //}
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        redrawScreen();
+    }
+
+    @Override
+    public void onClick(View view){
+
+        String viewName = intIdToString.get(view.getId());
+
+        //Code segment for car imagebutton click events
+        if(purchaser.isCarBought(viewName)){
+            purchaser.selectCar(viewName);
+        }
+        else if(purchaser.itemAffordable(viewName)){
+            purchaser.purchaseCar(viewName);
+            purchaser.selectCar(viewName);
+        }
+        //Code segment for Theme imagebutton click events
+        else if(purchaser.isThemeBought(viewName)){
+            purchaser.selectTheme(viewName);
+        }
+        else if(purchaser.itemAffordable(viewName)){
+            purchaser.purchaseTheme(viewName);
+            purchaser.selectTheme(viewName);
+        }
+        else{
+            //Intent ad = new Intent(this, advActivity.class);
+            //todo CALL JOAO's advert
+        }
+        redrawScreen();
+    }
+
+    /**
+     * Overriden method for when the user leaves the shopactivity,
+     * will save all changes made during the users shopping activity to
+     * the json database
+     */
+    @Override
+    public void onPause(){
+        super.onPause();
+        try {
+            purchaser.closeShop(this);
+        }catch(JSONException e){
+            Toast.makeText(this, "Problem saving to the database", Toast.LENGTH_SHORT);
+        }
+    }
 }
-
-
-
