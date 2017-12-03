e798dd13b16a3263f07d858fb644215437c0429f
Elena Kurganova <elena.kurganova@yahoo.com>
2017-12-04 00:48:06 +0100

    Fixed Exit PopUp

--- app/src/main/java/megadroid/drivinggame/view/exitButton.java
+++ app/src/main/java/megadroid/drivinggame/view/exitButton.java
@@ -1,38 +1,34 @@
 package megadroid.drivinggame.view;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import megadroid.drivinggame.R;
 
-/**
- * Created by Pemi on 2017-11-26.
- */
+
 
 
 public class exitButton extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.popupexit);
-
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-
-        getWindow().setLayout((int) (height * .6), (int) (width * .5));
-
     }
+
+
+
 
     @Override
     public void onClick(View v) {
