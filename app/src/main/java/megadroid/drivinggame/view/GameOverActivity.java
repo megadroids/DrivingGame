57f3b84f38c7c6ee3c68042b81866fbedfb2ce90
torinorgist <peiman.shokouhian@gmail.com>
2017-12-07 13:58:55 +0100

    Trasnparent Layout fixed Status bar showing up in game over now fixed

--- app/src/main/java/megadroid/drivinggame/view/GameOverActivity.java
+++ app/src/main/java/megadroid/drivinggame/view/GameOverActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import megadroid.drivinggame.R;
@@ -16,18 +17,24 @@ public class GameOverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_gameover);
         BackToMenu backToMenu = new BackToMenu();
         backToMenu.start();
         Intent intent = getIntent();
         boolean value = intent.getBooleanExtra("highscorebeaten",true);
         if (value) {
-            ImageView imageView = (ImageView) findViewById(R.id.gameoverimage);
-            imageView.setImageResource(R.drawable.gameover_beatenscore);
+            //ImageView imageView = (ImageView) findViewById(R.id.gameoverimage);
+            //imageView.setImageResource(R.drawable.gameover_beatenscore);
+            ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.gameoverlayout);
+            constraintLayout.setBackgroundResource(R.drawable.gameover_beatenscore);
         }
         else {
-            ImageView imageView = (ImageView) findViewById(R.id.gameoverimage);
-            imageView.setImageResource(R.drawable.gameover_notbeatenscore);
+            //ImageView imageView = (ImageView) findViewById(R.id.gameoverimage);
+            //imageView.setImageResource(R.drawable.gameover_notbeatenscore);
+            ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.gameoverlayout);
+            constraintLayout.setBackgroundResource(R.drawable.gameover_notbeatenscore);
         }
     }
 
